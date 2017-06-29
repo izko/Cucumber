64a69fdda674d2e884a2be23252ece9db8fe6f17
izko <ysabel93@gmail.com>
2017-06-29 11:09:11 +0200

    Add parallax header

--- js/out.js
+++ js/out.js
@@ -96,7 +96,26 @@ $(function () {
     $('#scroll').css({
       'transform': 'translate(0px, ' + wScroll / 20 + '%)'
     });
+
+    //menu scroll
+    if (wScroll > $('#secAdventures').offset().top) {
+      $('#homeHeader').addClass('is-showing');
+    } else {
+      $('#homeHeader').removeClass('is-showing');
+    }
   });
+
+  //  if(wScroll > $('#secAdventures').offset().top - ($(window).height() / 1.2)) {
+  //
+  //   $('.clothes-pics figure').each(function(i){
+  //
+  //     setTimeout(function(){
+  //       $('.clothes-pics figure').eq(i).addClass('is-showing');
+  //     }, 150 * (i+1));
+  //   });
+  //
+  // }
+
 });
 
 /***/ }),
--- js/parallax.js
+++ js/parallax.js
@@ -6,8 +6,27 @@ $(function()	{
     $('#scroll').css({
       'transform' : 'translate(0px, '+ wScroll /20 +'%)'
     });
+
+  //menu scroll
+  if (wScroll > $('#secAdventures').offset().top) {
+    $('#homeHeader').addClass('is-showing');
+  } else {
+    $('#homeHeader').removeClass('is-showing');
+  }
+
   });
 
+
+ //  if(wScroll > $('#secAdventures').offset().top - ($(window).height() / 1.2)) {
+ //
+ //   $('.clothes-pics figure').each(function(i){
+ //
+ //     setTimeout(function(){
+ //       $('.clothes-pics figure').eq(i).addClass('is-showing');
+ //     }, 150 * (i+1));
+ //   });
+ //
+ // }
 
 
 });
