b4b944e72ee86b25129bfe14de196288c2f35b0f
izko <ysabel93@gmail.com>
2017-07-08 13:01:10 +0200

    hamburger fixed

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,4 +3,14 @@ $(function()	{
     e.preventDefault();
     $('.menu').toggleClass('slide-down');
   });
-}); 
+
+  $('.hamburger').on('mouseenter', function(e) {
+    e.preventDefault();
+    $('.menu').addClass('slide-down');
+  });
+
+  $('ul a').on('click', function(e) {
+    e.preventDefault();
+    $('.menu').removeClass('slide-down');
+  });
+});
